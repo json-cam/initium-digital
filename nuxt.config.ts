ad99224f5fb730985914952ddae1209712bf5ca5
Jason Cameron <jasoncameron246@gmail.com>
2025-08-03 02:54:39 -0400

    Update bun.lockb dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxtjs/tailwindcss']
-})
+	modules: ["@nuxt/eslint", "@nuxt/image", "@nuxtjs/tailwindcss"],
+	devtools: { enabled: true },
+	compatibilityDate: "2025-07-15",
+	router: {
+		options: {
+			linkActiveClass: "active",
+			scrollBehaviorType: "smooth",
+		},
+	},
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+});
