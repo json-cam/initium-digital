a04c9139b88680c8dde1045057be887fc7074ccf
Jason Cameron <jasoncameron246@gmail.com>
2025-08-03 02:57:26 -0400

    Add app layouts directory and default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+        <NavBar />
+		<slot />
+        <FooterBar />
+	</div>
+</template>
+
+<style>
+   * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            min-width: fit-content;
+        }
+
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            line-height: 1.6;
+            color: #333;
+            /* background: linear-gradient(135deg, #667eea 0%, #764ba2 100%); */
+        }
+
+        .container {
+            max-width: 1600px;
+            margin: 0 auto;
+            padding: 0 20px;
+        }
+
+        
+         /* Animations */
+        @keyframes fadeInUp {
+            from {
+                opacity: 0;
+                transform: translateY(30px);
+            }
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+</style>
