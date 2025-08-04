1537c8bbcbcce4410ce10a9a1550548dc27e4c72
Jason Cameron <jasoncameron246@gmail.com>
2025-08-04 15:51:45 -0400

    Update Nuxt config with improved SEO meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,34 @@ export default defineNuxtConfig({
 	modules: ["@nuxt/eslint", "@nuxt/image", "@nuxtjs/tailwindcss"],
 	devtools: { enabled: true },
 	compatibilityDate: "2025-07-15",
+	app: {
+		head: {
+			title: "InitiumDigital",
+			htmlAttrs: {
+				lang: "en",
+			},
+			meta: [
+				{
+					charset: "utf-8",
+				},
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1, maximum-scale=1",
+				},
+				{
+					name: "InitiumDigital",
+					content: "Software Solutions, Web Development, and tech consulting services for small businesses, located in Thorold, Ontario. Empowering businesses with custom web and software solutions.",
+				}
+			],
+			link: [
+				{
+					rel: "icon",
+					type: "image/x-icon",
+					href: "/favicon.ico",
+				},
+			],
+		},
+	},
 	router: {
 		options: {
 			linkActiveClass: "active",
