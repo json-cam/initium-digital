51164bafb99631451ac15e72457ff99f530aa743
Jason Cameron <jasoncameron246@gmail.com>
2025-08-04 16:21:18 -0400

    removed tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/eslint", "@nuxt/image", "@nuxtjs/tailwindcss"],
+	modules: ["@nuxt/eslint", "@nuxt/image"],
 	devtools: { enabled: true },
 	compatibilityDate: "2025-07-15",
 	app: {
