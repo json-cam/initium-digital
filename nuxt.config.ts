b6b1d437861f57ea14b8500180b178c28e6e7e7c
Jason Cameron <jasoncameron246@gmail.com>
2025-10-01 14:56:57 -0400

    Update nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,11 @@ export default defineNuxtConfig({
 		},
 	},
 	compatibilityDate: "2025-07-15",
+	nitro: {
+		prerender: {
+			routes: ["/", "/#contact"],
+		},
+	},
 	eslint: {
 		config: {
 			stylistic: {
